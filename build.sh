0f5e8ab9d48b64e6c6e40ade288bdfc5fac067b2
aswijaadapa <aswijaadapa16@gmail.com>
2026-07-17 15:04:09 +0530

    Fix build.sh with course loading, model training, and superuser creation

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py load_courses
+python manage.py train_model
+python manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" || true
